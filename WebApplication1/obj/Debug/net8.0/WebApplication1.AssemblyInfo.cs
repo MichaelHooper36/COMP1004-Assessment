c596b09a88446c6b6ca75a5ac72c24eb5de7e0ac
Michael Hooper <mikeyhooper360@gmail.com>
2024-03-19 12:19:46 +0000

    Update WebApplication1.AssemblyInfo.cs

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12880799c7c6a47f0c49cc2d985ae56813782415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029328422c9e026cabb28465857aff4db77b0a11")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
