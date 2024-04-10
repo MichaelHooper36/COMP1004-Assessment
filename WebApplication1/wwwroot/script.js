17dc8a1e4c6ae3618dc357039ef8da2c17f8e2b7
Michael Hooper <mikeyhooper360@gmail.com>
2024-04-10 12:27:58 +0100

    Make stuff appear

--- WebApplication1/wwwroot/script.js
+++ WebApplication1/wwwroot/script.js
@@ -7,3 +7,20 @@ var arr2 = ["#Minotaur", "#Theseus", "#Athens", "#Deadalus", "#Wings"];
 function randSite2() {
     location.href = arr2[Math.floor(Math.random() * arr2.length)];
 }
+
+var arr3 = [];
+function randSite3() {
+    location.href = arr3[Math.floor(Math.random() * arr3.length)];
+}
+
+function giantFunc() 
+{
+    document.getElementById("Giant").classList.remove("hidden");
+    document.getElementById("Giant").classList.add("show");
+}
+
+function jormunFunc() 
+{
+    document.getElementById("Jormungand").classList.remove("hidden");
+    document.getElementById("Jormungand").classList.add("show");
+}
